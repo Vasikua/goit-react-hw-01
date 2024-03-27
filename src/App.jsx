6ac8e033c99c1f8d8ec5e38a406d1829af77017d
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-27 00:22:46 -0500

    history begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
-import userData from "../userData.json";
-import friends from "../friends.json";
+import userData from "./userData.json";
+import friends from "./friends.json";
+import transaction from "./transactions.json";
 import Profile from "./components/Profile/Profile";
 import FriendList from "./components/FriendList/FriendList"
-// import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
     <>
       < Profile data={userData} />
       <FriendList data={friends} />
-      {/* <TransactionHistory data ={} /> */}
+      <TransactionHistory data ={transaction} />
     </>
   )
 }
