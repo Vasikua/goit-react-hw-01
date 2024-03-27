88c380b60b136d94ae73394e29eef7b265767ab6
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-26 22:04:51 -0500

    friend list begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import './App.css'
-import userdata from "../src/userdata.json" 
-import Profile from "./components/Profile/Profile"
+import './App.css';
+import userdata from "./userdata.json"; 
+import Profile from "./components/Profile/Profile";
 export default function App() {
   return (
-    //scdvsfbgnhgbfvdcsxa
+    
     <>
      < Profile data = {userdata}/>
     </>
