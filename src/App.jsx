770ac26ab0f4d6905802630ebb202900d8d14471
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-26 21:44:11 -0500

    adew1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import userdata from "../src/userdata.json"
 import Profile from "./components/Profile/Profile"
 export default function App() {
   return (
+    //scdvsfbgnhgbfvdcsxa
     <>
      < Profile data = {userdata}/>
     </>
