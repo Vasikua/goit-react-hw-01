29415effd24bcb443f4251282f96aa34416c1ede
Vasyl Mandik <Vasik_ua@ukr.net>
2024-03-28 00:09:09 -0500

    Added untracked files

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,15 @@
+import FriendListCard from "./FriendListCard";
+import css from "./FriendList.module.css";
+export default function FriendList({data}) {
+    return (
+
+        <ul className={css.list}>
+            {data.map((friend) => (
+                <li className={css.item} key={friend.id}>
+                    <FriendListCard data={friend} />
+                </li>
+            ))}
+            
+        </ul>);
+  
+}
